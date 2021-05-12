e317306dde1076c49c4be67f035aa79bc482bc2a
CaioodosSantos <Ccaio.456@gmail.com>
2021-05-11 21:01:49 -0300

    import seeds
    criei seed usuario

---
+++ database/seeds/100UsuarioSeeder.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| UsuarioSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class UsuarioSeeder {
+  async run () {
+  }
+}
+
+module.exports = UsuarioSeeder
