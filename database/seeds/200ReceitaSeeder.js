85e5a84c96e7643f61a4572ab594885c040349ee
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2021-05-11 21:06:56 -0300

    import seed
    criei o arquivo da seed receita

---
+++ database/seeds/200ReceitaSeeder.js
@@ -0,0 +1,21 @@
+'use strict'
+
+/*
+|--------------------------------------------------------------------------
+| 200ReceitaSeeder
+|--------------------------------------------------------------------------
+|
+| Make use of the Factory instance to seed database with dummy data or
+| make use of Lucid models directly.
+|
+*/
+
+/** @type {import('@adonisjs/lucid/src/Factory')} */
+const Factory = use('Factory')
+
+class 200ReceitaSeeder {
+  async run () {
+  }
+}
+
+module.exports = 200ReceitaSeeder
