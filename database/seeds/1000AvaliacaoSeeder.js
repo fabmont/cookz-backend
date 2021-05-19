c958c889160484fe7cd293549d5dff689937bc84
hackerxxx1 <43994798+hackerxxx1@users.noreply.github.com>
2021-05-19 01:44:50 -0300

    seed de avaliação
    nesse comiit leva a seed de avaliação com uma nota de 0 a 10 de algumas receitas

--- database/seeds/1000AvaliacaoSeeder.js
+++ database/seeds/1000AvaliacaoSeeder.js
@@ -19,12 +19,12 @@ class AvaliacaoSeeder {
   async run () {
 
     await Avaliacao.createMany([
-      {id: 1, comentario: '',nota: ''},
-      {id: 2, comentario: '',nota: ''},
-      {id: 3, comentario: '',nota: ''},
-      {id: 4, comentario: '',nota: ''},
-      {id: 5, comentario: '',nota: ''},
-      {id: 6, comentario: '',nota: ''},
+      {id: 1, comentario: 'O brownie e gostoso viciante, toda vez que come um pedaço voce quer comer mais',nota: 10,id_usuario:2,id_receita:1},
+      {id: 2, comentario: 'peixe e ruim em si, a receita tenta deixar o peixe mais gostoso porem continua ruim',nota: 0,id_usuario:2,id_receita:2},
+      {id: 3, comentario: 'arroz com carne e muito bom',nota: 7,id_usuario:2,id_receita:3},
+      {id: 4, comentario: 'muito gostoso saboroso',nota: 7,id_usuario:2,id_receita:4},
+      {id: 5, comentario: 'churros e perfeito maravilhoso',nota: 10,id_usuario:2,id_receita:6},
+      {id: 6, comentario: 'muito gostoso viciante o gosto e de dar agua na boca',nota: 10,id_usuario:2,id_receita:7},
 
     ])
   }
