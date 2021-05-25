d66096514798ec2acadb2bdf963faa4a7d60e2a8
fabmont <fabriciosantos47@gmail.com>
2021-05-24 21:12:12 -0300

    style: ajusta identação do modulo de favoritos

--- app/Controllers/Http/FavoritoController.js
+++ app/Controllers/Http/FavoritoController.js
@@ -1,44 +1,35 @@
-const Favorito = use('App/Models/Favorito') 
+const Favorito = use('App/Models/Favorito');
 
 class FavoritoController {
-
-  async index ({ request, response, view }) {
-    let {page, perPage} = request.all()
-    return Favorito.query().paginate(page, perPage)
-    }
-
-  async store ({ request, response }) {
-
-    const campos = Favorito.getCamposCadastro()
-    const dados = request.only(campos)
-
-    return await Favorito.create(dados)
+  async index({ request, response, view }) {
+    let { page, perPage } = request.all();
+    return Favorito.query().paginate(page, perPage);
   }
 
-  async show ({ params, request, response, view }) {
+  async store({ request, response }) {
+    const campos = Favorito.getCamposCadastro();
+    const dados = request.only(campos);
 
-    return await Favorito.query()
-                          .where('id', params.id)
-                          .with('')
-                          .first()
+    return await Favorito.create(dados);
   }
 
-  async update ({ params, request, response }) {
-
-    const favorito = await Favorito.findOrFail(params.id)
-
-    const campos = Favorito.getCamposCadastro()
-    const dados = request.only(campos)
-
-    favorito.merge(dados)
-    await  favorito.save()
-    return favorito
+  async show({ params, request, response, view }) {
+    return await Favorito.query().where('id', params.id).with('').first();
   }
 
+  async update({ params, request, response }) {
+    const favorito = await Favorito.findOrFail(params.id);
 
-  async destroy ({ params, request, response }) {
+    const campos = Favorito.getCamposCadastro();
+    const dados = request.only(campos);
 
-    const favorito = await Favorito.findOrFail(params.id)
+    favorito.merge(dados);
+    await favorito.save();
+    return favorito;
+  }
+
+  async destroy({ params, request, response }) {
+    const favorito = await Favorito.findOrFail(params.id);
     return await favorito.delete();
   }
 }
